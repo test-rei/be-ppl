8c2f52ad4e9f5d9041055ae10bfee1d7f7af3946
Rai Pramana <rai.pramana46@gmail.com>
2024-11-09 22:16:05 +0800

    modified:   models/index.js

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 import sequelize from "../config/database.js";
-import { MK } from "./mk.js";
-import { KRS } from "./krs.js";
-import { MHS } from "./mhs.js";
-import { IPK } from "./ipk.js";
+import MK from "./mk.js";
+import KRS from "./krs.js";
+import MHS from "./mhs.js";
+import IPK from "./ipk.js";
 
 // Relasi antar tabel (Jika ada relasi)
 MHS.hasMany(KRS, {
