c3f537feda44fa00c140a7c61c8926388dc43a25
Rai Pramana <rai.pramana46@gmail.com>
2024-11-12 22:32:12 +0800

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import sequelize from "./config/database.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ import ipkRoutes from "./routes/ipkRoutes.js";
 
 app.use(express.json({}));
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use(mkRoutes);
 app.use(krsRoutes);
