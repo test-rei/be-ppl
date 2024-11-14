f3150f9fd1624ac56de3ce8e5ed2e8cb9c2e717f
Rai Pramana <rai.pramana46@gmail.com>
2024-11-14 18:23:55 +0800

    modified:   routes/ipkRoutes.js

--- routes/ipkRoutes.js
+++ routes/ipkRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getAllIPK, getIPKById, createIPK, updateIPK, deleteIPK } from "../controllers/ipkController.js";
+import { getAllIPK, getIPKById, getAllIPKCalculate, createIPK, updateIPK, deleteIPK } from "../controllers/ipkController.js";
 
 const router = express.Router();
 
 router.get("/ipk", getAllIPK);
 router.get("/ipk/:id", getIPKById);
+router.get("/ipkc", getAllIPKCalculate);
 router.post("/ipk", createIPK);
 router.patch("/ipk/:id", updateIPK);
 router.delete("/ipk/:id", deleteIPK);
